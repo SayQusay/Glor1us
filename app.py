b3fe59944cdfa1576baecebbbf01d472403435c5
SayQusay <qusayakib24@gmail.com>
2024-09-10 15:26:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,12 +30,14 @@ import pickle
 import joblib
 import os
 import glob
-import gdown
+import requests
 
-# Fungsi untuk mengunduh file dari Google Drive
-def download_from_gdrive(file_id, output_path):
-    gdown.download(f"https://itsacid.sharepoint.com/:u:/r/sites/BismillahGEMASTIK/Dokumen%20Berbagi/General/Streamlit%20-%20(ALL%20DONE)%20-%20Hover/vgg16_feature_extractor.pth?csf=1&web=1&e=7EqEbY", output_path, quiet=False)
+# Fungsi untuk mengunduh file dari OneDrive/SharePoint
+def download_from_teams(url, output_path):
+    # Menggunakan requests untuk mengunduh file
+    response = requests.get(url)
 
+file_url = 'https://itsacid.sharepoint.com/:u:/r/sites/BismillahGEMASTIK/Dokumen%20Berbagi/General/Streamlit%20-%20(ALL%20DONE)%20-%20Hover/vgg16_feature_extractor.pth?csf=1&web=1&e=7EqEbY'  # Ganti dengan URL file dari Teams
 # ID file dari Google Drive (ganti dengan file ID milik Anda)
 output_path = 'vgg16_feature_extractor.pth'  # Nama file yang akan disimpan di lokal
 
